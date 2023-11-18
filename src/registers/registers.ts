af25185a4ef9a47dab28342ef098d23ced8086b3
Jesús Bovea <bovea3359@gmail.com>
2023-11-18 19:14:52 -0400

    adds optimized indexes calculations

--- src/registers/registers.ts
+++ src/registers/registers.ts
@@ -21,11 +21,19 @@ let newStruct = (
   types: string[],
 ): Type[] | null => {
   if (types.length == 0) return null;
-  let programTypes = program.filter((element) => types.includes(element.name));
-
-  types.forEach((typeName) => {
+  if (program.find((el) => el.name == name) != undefined) return null;
+  for (let i = 0; i < types.length; i++) {
+    let typeName = types[i];
     if (program.find((el) => el.name == typeName) == undefined) return null;
-  });
+  }
+
+  let programTypes: Type[] = [];
+
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < program.length; j++) {
+      if (types[i] == program[j].name) programTypes.push(program[j]);
+    }
+  }
 
   let newStruct: Struct = {
     name,
@@ -41,15 +49,29 @@ let newUnion = (
   name: string,
   types: string[],
 ): Type[] | null => {
-  let programTypes = program.filter((element) => types.includes(element.name));
-
-  types.forEach((typeName) => {
+  if (types.length == 0) return null;
+  if (program.find((el) => el.name == name) != undefined) return null;
+  for (let i = 0; i < types.length; i++) {
+    let typeName = types[i];
     if (program.find((el) => el.name == typeName) == undefined) return null;
+  }
+
+  let programTypes: Type[] = [];
+
+  for (let i = 0; i < types.length; i++) {
+    for (let j = 0; j < program.length; j++) {
+      if (types[i] == program[j].name) programTypes.push(program[j]);
+    }
+  }
+
+  let flatTypes = plainRegister(programTypes);
+  let biggestType = flatTypes.reduce((prev, current) => {
+    return (prev as Atomic).rep > (current as Atomic).rep ? prev : current;
   });
 
   let newUnion: Union = {
     name,
-    types: programTypes,
+    types: [biggestType],
   };
 
   program.push(newUnion);
@@ -58,22 +80,23 @@ let newUnion = (
 
 const isAtomic = (val: Type): val is Atomic => !!(val as Atomic)?.rep;
 
-const getUnpacked = (types: Type[]): [number, number][] => {
+const getUnpackedR = (types: Type[], base: number): [number, number][] => {
   let blockArr: [number, number][] = isAtomic(types[0])
-    ? [[0, types[0].rep]]
-    : getUnpacked(types[0].types);
+    ? [[base, base + types[0].rep]]
+    : getUnpackedR(types[0].types, base);
 
-  types.forEach((type) => {
+  types.slice(1, types.length).forEach((type) => {
+    let lastEndIndex = base + blockArr[blockArr.length - 1][1];
     if (!isAtomic(type)) {
-      blockArr.push(...getUnpacked(type.types));
+      let registerIndexes = getUnpackedR(type.types, lastEndIndex);
+      blockArr.push(...registerIndexes);
       return;
     }
 
-    let lastEndIndex = blockArr[blockArr.length - 1][1];
     let currentType = type as Atomic;
 
     for (let i = 0; ; i++) {
-      let currentIndex = currentType.rep * i;
+      let currentIndex = currentType.alignment * i;
       if (currentIndex >= lastEndIndex) {
         blockArr.push([currentIndex, currentIndex + currentType.rep]);
         break;
@@ -84,31 +107,87 @@ const getUnpacked = (types: Type[]): [number, number][] => {
   return blockArr;
 };
 
-const getPacked = (types: Type[]): [number, number][] => {
-  let blockArr: [number, number][] = isAtomic(types[0])
-    ? [[0, types[0].rep]]
-    : getUnpacked(types[0].types);
-
-  return blockArr;
+const getUnpacked = (types: Type[]): [number, number][] => {
+  return getUnpackedR(types, 0);
 };
 
-const getOptimized = (types: Type[]): [number, number][] => {
+const getPackedR = (types: Type[], base: number): [number, number][] => {
   let blockArr: [number, number][] = isAtomic(types[0])
-    ? [[0, types[0].rep]]
-    : getUnpacked(types[0].types);
+    ? [[base, base + types[0].rep]]
+    : getPackedR(types[0].types, base);
 
-  types.forEach((type) => {
+  types.slice(1, types.length).forEach((type) => {
+    let lastEndIndex = base + blockArr[blockArr.length - 1][1];
     if (!isAtomic(type)) {
-      blockArr.push(...getUnpacked(type.types));
+      let registerIndexes = getPackedR(type.types, lastEndIndex);
+      blockArr.push(...registerIndexes);
       return;
     }
 
-    let lastEndIndex = blockArr[blockArr.length - 1][1];
     let currentType = type as Atomic;
     blockArr.push([lastEndIndex, lastEndIndex + currentType.rep]);
   });
 
   return blockArr;
+};
+
+const getPacked = (types: Type[]): [number, number][] => {
+  return getPackedR(types, 0);
+};
+
+const plainRegister = (types: Type[]): Type[] => {
+  let plainTypes: Type[] = [];
+
+  types.forEach((type) => {
+    if (!isAtomic(type)) {
+      let tempPlain = plainRegister(type.types);
+      plainTypes.push(...tempPlain);
+    } else {
+      plainTypes.push(type);
+    }
+  });
+
+  return plainTypes;
+};
+const getOptimizedR = (types: Type[], base: number): [number, number][] => {
+  let list = [...plainRegister(types)];
+  let blockArr: [number, number][] = isAtomic(list[0])
+    ? [[base, base + list[0].rep]]
+    : getOptimizedR(list[0].types, base);
+
+  list.splice(0, 1);
+
+  while (list.length != 0) {
+    let lastEndIndex = base + blockArr[blockArr.length - 1][1];
+    let currentListSize = list.length;
+
+    while (list.length == currentListSize) {
+      find: for (let i = 0; i < list.length; i++) {
+        let type = list[i];
+        if (!isAtomic(type)) {
+          blockArr.push(...getOptimizedR(type.types, lastEndIndex));
+          list.splice(i, 1);
+          break find;
+        } else {
+          for (let j = 0; type.alignment * j <= lastEndIndex; j++) {
+            if (type.alignment * j == lastEndIndex) {
+              blockArr.push([lastEndIndex, lastEndIndex + type.rep]);
+              list.splice(i, 1);
+              break find;
+            }
+          }
+        }
+      }
+
+      lastEndIndex++;
+    }
+  }
+
+  return blockArr;
+};
+
+const getOptimized = (types: Type[]): [number, number][] => {
+  return getOptimizedR(types, 0);
 };
 
 const getWastedBlocks = (blocks: [number, number][]) => {
@@ -125,7 +204,7 @@ export {
   newStruct,
   newUnion,
   getUnpacked,
-  getPacked,
   getOptimized,
+  getPacked,
   getWastedBlocks,
 };
